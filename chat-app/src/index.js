64e2bc69cbcf49f1b1ef4372d21bcc034348f4a1
Khoa Vo <khoa.vo@s3corp.com.vn>
2020-09-09 17:17:35 +0700

    update chat-app: handling websocket

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,7 +1,38 @@
 const express = require('express')
+const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000')
+const port = process.env.PORT || 3000
+const publicDirectory = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectory))
+
+io.on('connection', socket => {
+  console.log('WebSocket connection')
+  socket.emit('message', 'Welcome!')
+  socket.broadcast.emit('message', 'A new user has joined!')
+
+  socket.on('sendMessage', message => {
+    io.emit('message', message)
+  })
+
+  socket.on('sendLocation', coords => {
+    io.emit(
+      'message',
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    )
+  })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!')
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
 })
