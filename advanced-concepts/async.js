39333532b287c3e612951a40def672a8dc696b6b
Khoa Vo <khoa.vo@s3corp.com.vn>
2020-09-18 16:27:21 +0700

    update advanced-concepts

--- advanced-concepts/async.js
+++ advanced-concepts/async.js
@@ -2,9 +2,17 @@ const https = require('https')
 
 const start = Date.now()
 
-https.request('https://www.google.com.vn', res => {
-  res.on('data', () => {})
-  res.on('end', () => {
-    console.log(Date.now() - start)
-  })
-}).end()
+function doRequest() {
+  https.request('https://www.google.com.vn', res => {
+    res.on('data', () => {})
+    res.on('end', () => {
+      console.log(Date.now() - start)
+    })
+  }).end()
+}
+
+doRequest()
+doRequest()
+doRequest()
+doRequest()
+doRequest()
